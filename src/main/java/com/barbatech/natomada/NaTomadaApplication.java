d732f67b10a064d02ad7ae7a746d9901c1a372dd
Barba2k2 <lorenzo.dz@hotmail.com>
2025-10-28 11:31:38 -0300

    Initializing project

---
+++ src/main/java/com/barbatech/natomada/NaTomadaApplication.java
@@ -0,0 +1,13 @@
+package com.barbatech.natomada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaTomadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaTomadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbatech/natomada/NaTomadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbatech.natomada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaTomadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
