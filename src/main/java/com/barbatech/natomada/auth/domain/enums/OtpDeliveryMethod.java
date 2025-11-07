0684676622c558a2130e39ed486e39b8c43a55b0
Barba2k2 <lorenzo.dz@hotmail.com>
2025-11-07 20:35:34 -0300

    feat: add OTP delivery method enum

---
+++ src/main/java/com/barbatech/natomada/auth/domain/enums/OtpDeliveryMethod.java
@@ -0,0 +1,16 @@
+package com.barbatech.natomada.auth.domain.enums;
+
+/**
+ * Enum for OTP delivery methods
+ */
+public enum OtpDeliveryMethod {
+    /**
+     * Send OTP via email
+     */
+    EMAIL,
+
+    /**
+     * Send OTP via SMS
+     */
+    SMS
+}
