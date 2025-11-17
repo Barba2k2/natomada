963db0e2bf288349906aabb9386a5a462a69da19
Barba2k2 <lorenzo.dz@hotmail.com>
2025-11-17 19:24:35 -0300

    feat: add review dtos

---
+++ src/main/java/com/barbatech/natomada/reviews/application/dtos/CreateReviewRequestDto.java
@@ -0,0 +1,25 @@
+package com.barbatech.natomada.reviews.application.dtos;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO for creating a new review
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateReviewRequestDto {
+
+    @NotNull(message = "Avaliação é obrigatória")
+    @Min(value = 1, message = "Avaliação mínima é 1")
+    @Max(value = 5, message = "Avaliação máxima é 5")
+    private Integer rating;
+
+    @Size(max = 1000, message = "Comentário não pode ter mais de 1000 caracteres")
+    private String comment;
+}
---
+++ src/main/java/com/barbatech/natomada/reviews/application/dtos/ReviewResponseDto.java
@@ -0,0 +1,43 @@
+package com.barbatech.natomada.reviews.application.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * DTO for review response
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewResponseDto {
+
+    private Long id;
+
+    @JsonProperty("station_id")
+    private Long stationId;
+
+    @JsonProperty("user_id")
+    private Long userId;
+
+    @JsonProperty("user_name")
+    private String userName;
+
+    @JsonProperty("user_profile_image")
+    private String userProfileImage;
+
+    private Integer rating;
+
+    private String comment;
+
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
+    private LocalDateTime updatedAt;
+}
