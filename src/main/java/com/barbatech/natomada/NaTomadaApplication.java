73e86e78d9a352eccfcedcf1de138fd15aec4aeb
Barba2k2 <lorenzo.dz@hotmail.com>
2025-11-30 16:04:50 -0300

    chore: enable scheduling for station sync

--- src/main/java/com/barbatech/natomada/NaTomadaApplication.java
+++ src/main/java/com/barbatech/natomada/NaTomadaApplication.java
@@ -2,8 +2,10 @@ package com.barbatech.natomada;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NaTomadaApplication {
 
 	public static void main(String[] args) {
